f7bc0eeedca63aba66b39ed17dd01cd4af5e4e9c
Daniel Capua <danielcapua19@gmail.com>
2023-05-07 20:45:28 -0500

    Updated .gitignore and added GuessTheFlag project.

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Danny Capua on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
