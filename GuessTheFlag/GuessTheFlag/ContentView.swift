cb07534c0c44eae53b5bcd7a1e6c9f871d8e4251
Daniel Capua <danielcapua19@gmail.com>
2023-05-26 23:10:38 -0500

    extended GuessTheFlag app

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
+    @State private var showingFinalAlert = false
     @State private var scoreTitle = ""
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland","Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var userScore = 0
+    @State private var gameCount = 0
     
     
     var body: some View {
@@ -21,7 +25,7 @@ struct ContentView: View {
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.3), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
-            .ignoresSafeArea()
+            .ignoresSafeArea() // goes above dynamic island
             
             VStack {
                 Spacer()
@@ -32,7 +36,7 @@ struct ContentView: View {
                 VStack (spacing: 15){
                     VStack{
                         Text("Tap the flag of")
-                            .foregroundColor(.secondary)
+                            .foregroundColor(.secondary) // Color.primary or secondary is a semantic color that changes based on the device theme
                             .font(.subheadline.weight(.heavy))
                         Text(countries[correctAnswer])
                             .foregroundColor(.white)
@@ -52,24 +56,31 @@ struct ContentView: View {
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
-                .background(.regularMaterial)
+                .background(.regularMaterial) // applies a frosted glass-style blur to the background
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
                 Spacer()
+                
+                Text("Game: \(gameCount)/8")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
             }
             .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore){ // showingShore is changed when any flag is tapped
-            Button("Continue", action: askQuestion)
+            Button("Continue", action: askQuestion) // goes to next question
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(userScore)")
+        }
+        .alert("Final game. Resetting...", isPresented: $showingFinalAlert){
+            Button("Start Next Game", action: reset)
         }
         
     }
@@ -77,16 +88,28 @@ struct ContentView: View {
     func flagTapped(_ number: Int){
         if number == correctAnswer{
             scoreTitle = "Correct"
+            userScore+=1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, that's the flag of \(countries[number])"
         }
         
         showingScore = true
+        gameCount+=1
+        
+        if gameCount == 8 {
+            showingFinalAlert = true
+        }
     }
     
     func askQuestion(){
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset(){
+        userScore = 0
+        gameCount = 0
+        askQuestion()
     }
 }
 
